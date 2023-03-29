8a495e89182b6aa505ca4ffe1098dec904b4e2c2
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-03-29 00:53:10 -0300

    starting code

---
+++ main.java
@@ -0,0 +1,9 @@
+import org.catolicasc.bank.maruan.Endereco;
+
+public class main {
+
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        System.out.println(endereco);
+    } 
+}
